7d5d029a1236f7dee77e4efeabe9b1c3ae35a7bc
Jogis <ronipihajoki@proton.me>
2025-11-13 19:06:00 +0200

    .

--- AutoTehtava/src/App.java
+++ AutoTehtava/src/App.java
@@ -1,5 +1,49 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+    
+    
     }
 }
+class auto{
+    public String merkki;
+    public String malli;
+    public int bensaMaara;
+
+public void jarruta(){
+
+}
+
+public void kiihdyta(){
+
+}
+
+public void naytaTiedot(){
+
+}
+}
+
+/*
+1. Tee Auto-luokka.
+
+2. Määrittele autoluokalle attribuutit: merkki, malli, bensanMaara. Aseta kaikkein attribuuttien näkyvyydeksi public.
+
+3. Määrittele metodit:
+
+a. Jarruta: Metodi tulostaa konsoliin”Auto jarruttaa”.
+
+b. Kiihdyta: Metodi tulostaa konsoliin ”Auto kiihtyy” ja bensanMaara-attribuutin arvoa vähennetään yhdellä.
+
+c. NaytaTiedot: Metodi tulostaa konsoliin merkin, mallin ja bensan määrän.
+
+4. Luo auto1-olio ja anna sille arvot merkki, malli ja bensanMaara. Tulosta auton tiedot, kiihdytä autoa ja tulosta uudestaan.
+
+5. Kokeile lisätä kiihdyta-metodiin ehto, että bensanMaara tulee olla enemmän kuin 0.
+
+6. Lisää Auto-luokkaan tankkaa-metodi, jolla bensan määrä saadaan asetettua. Eli tankkaa-metodi saa attribuuttina määrän paljon tankattiin. Huomio tankissa olemassa oleva bensan määrä. Voit esim. metodissa tulostaa seuraavasti: Tankissa bensaa: 10 Tankkaus: 5 ß tämä on saadun parametrin arvo Tankissa bensaa tankkauksen jälkeen: 15
+
+7. Tee konstruktori/muodostin, jolla on kolme parametria: merkki, malli ja bensan määrä. Kun teet parametrisen muodostimen, sinun täytynee tehdä myös parametriton muodostin...
+
+8. Luo auto2-olio käyttäen edellä tekemääsi konstruktoria.
+ */
